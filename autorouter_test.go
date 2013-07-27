7ea67b5534b911c2ae72ba41722563e525b06a68
Unknown <joe2010xtmf@163.com>
2013-07-27 09:44:44 +0800

    Added func getControllerInfo

---
+++ autorouter_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetControllerInfo(t *testing.T) {
+	getControllerInfo("testdata/router/")
+}
---
+++ testdata/router/router.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type Router struct {
+	beego.Controller
+}
+
+func (this *Router) Get() {
+
+}
+
+func (this *Router) Post() {
+
+}
+
+type Controller struct {
+}
+
+func (this *Controller) Put() {
+
+}
+
+func (this *Controller) Delete() {
+
+}
