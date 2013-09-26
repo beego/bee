33ff8482f76ee7a2b5151ed111bf91bb5009d983
Odiel Leon <odiel.leon@gmail.com>
2013-09-25 23:22:35 -0400

    - Moving <style> tag under <head> tag

--- new.go
+++ new.go
@@ -154,62 +154,62 @@ var indextpl = `<!DOCTYPE html>
   	<head>
     	<title>Beego</title>
     	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  	</head>
 	
-	<style type="text/css">
-		body {
-			margin: 0px;
-			font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
-			font-size: 14px;
-			line-height: 20px;
-			color: rgb(51, 51, 51);
-			background-color: rgb(255, 255, 255);
-		}
+		<style type="text/css">
+			body {
+				margin: 0px;
+				font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+				font-size: 14px;
+				line-height: 20px;
+				color: rgb(51, 51, 51);
+				background-color: rgb(255, 255, 255);
+			}
 
-		.hero-unit {
-			padding: 60px;
-			margin-bottom: 30px;
-			border-radius: 6px 6px 6px 6px;
-		}
+			.hero-unit {
+				padding: 60px;
+				margin-bottom: 30px;
+				border-radius: 6px 6px 6px 6px;
+			}
 
-		.container {
-			width: 940px;
-			margin-right: auto;
-			margin-left: auto;
-		}
+			.container {
+				width: 940px;
+				margin-right: auto;
+				margin-left: auto;
+			}
 
-		.row {
-			margin-left: -20px;
-		}
+			.row {
+				margin-left: -20px;
+			}
 
-		h1 {
-			margin: 10px 0px;
-			font-family: inherit;
-			font-weight: bold;
-			text-rendering: optimizelegibility;
-		}
+			h1 {
+				margin: 10px 0px;
+				font-family: inherit;
+				font-weight: bold;
+				text-rendering: optimizelegibility;
+			}
 
-		.hero-unit h1 {
-			margin-bottom: 0px;
-			font-size: 60px;
-			line-height: 1;
-			letter-spacing: -1px;
-			color: inherit;
-		}
+			.hero-unit h1 {
+				margin-bottom: 0px;
+				font-size: 60px;
+				line-height: 1;
+				letter-spacing: -1px;
+				color: inherit;
+			}
 
-		.description {
-		    padding-top: 5px;
-		    padding-left: 5px;
-		    font-size: 18px;
-		    font-weight: 200;
-		    line-height: 30px;
-		    color: inherit;
-		}
+			.description {
+				padding-top: 5px;
+				padding-left: 5px;
+				font-size: 18px;
+				font-weight: 200;
+				line-height: 30px;
+				color: inherit;
+			}
 
-		p {
-		    margin: 0px 0px 10px;
-		}
-	</style>
+			p {
+				margin: 0px 0px 10px;
+			}
+		</style>
+	</head>
   	
   	<body>
   		<header class="hero-unit" style="background-color:#A9F16C">
