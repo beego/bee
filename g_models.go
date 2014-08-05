d02a552a59634d239436f472c980b463d132728f
ZhengYang <zhengyang4k@gmail.com>
2014-08-05 16:13:40 +0800

    fix doc format bug, use only 1 tab in between fields

--- g_models.go
+++ g_models.go
@@ -756,12 +756,12 @@ func (this *{{ctrlName}}Controller) GetOne() {
 
 // @Title Get All
 // @Description get {{ctrlName}}
-// @Param	query		query 	string	false		"Filter. e.g. col1:v1,col2:v2 ..."
-// @Param	fields		query	string	false		"Fields returned. e.g. col1,col2 ..."
-// @Param	sortby		query	string	false		"Sorted-by fields. e.g. col1,col2 ..."
-// @Param	order		query	string	false		"Order corresponding to each sortby field, if single value, apply to all sortby fields. e.g. desc,asc ..."
-// @Param	limit		query	string	false		"Limit the size of result set. Must be an integer"
-// @Param	offset		query	string	false		"Start position of result set. Must be an integer"
+// @Param	query	query	string	false	"Filter. e.g. col1:v1,col2:v2 ..."
+// @Param	fields	query	string	false	"Fields returned. e.g. col1,col2 ..."
+// @Param	sortby	query	string	false	"Sorted-by fields. e.g. col1,col2 ..."
+// @Param	order	query	string	false	"Order corresponding to each sortby field, if single value, apply to all sortby fields. e.g. desc,asc ..."
+// @Param	limit	query	string	false	"Limit the size of result set. Must be an integer"
+// @Param	offset	query	string	false	"Start position of result set. Must be an integer"
 // @Success 200 {object} models.{{ctrlName}}
 // @Failure 403 
 // @router / [get]
