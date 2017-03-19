7811c335e9bb04cccedb2d32e5104ebf3da072d4
Faissal Elamraoui <amr.faissal@gmail.com>
2017-03-19 23:45:54 +0100

    Update vendor folder (Delve support)

---
+++ vendor/github.com/derekparker/delve/pkg/proc/doc.go
@@ -0,0 +1,9 @@
+// Package proc is a low-level package that provides methods to manipulate
+// the process we are debugging.
+//
+// proc implements all core functionality including:
+// * creating / attaching to a process
+// * process manipulation (step, next, continue, halt)
+// * methods to explore the memory of the process
+//
+package proc
---
+++ vendor/github.com/derekparker/delve/pkg/proc/exec_darwin.h
@@ -0,0 +1,10 @@
+#include "proc_darwin.h"
+
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int
+fork_exec(char *, char **, int, char *, task_t*, mach_port_t*, mach_port_t*, mach_port_t*);
