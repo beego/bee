963e1c9d058351f5f77cd9ec3140b5598770078a
astaxie <xiemengjun@gmail.com>
2017-04-24 21:11:32 +0800

    bee 1.8.3

--- cmd/commands/version/version.go
+++ cmd/commands/version/version.go
@@ -57,7 +57,7 @@ Prints the current Bee, Beego and Go version alongside the platform information.
 }
 var outputFormat string
 
-const version = "1.8.2"
+const version = "1.8.3"
 
 func init() {
 	fs := flag.NewFlagSet("version", flag.ContinueOnError)
