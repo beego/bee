2ef722e7584f171e8d8df76fc72ce13e77f1f7f7
MZI <macchenjl@foxmail.com>
2018-10-13 21:46:20 +0800

    Fix delve funcs

--- cmd/bee.go
+++ cmd/bee.go
@@ -20,6 +20,7 @@ import (
 	_ "github.com/beego/bee/cmd/commands/api"
 	_ "github.com/beego/bee/cmd/commands/bale"
 	_ "github.com/beego/bee/cmd/commands/beefix"
+	_ "github.com/beego/bee/cmd/commands/dlv"
 	_ "github.com/beego/bee/cmd/commands/dockerize"
 	_ "github.com/beego/bee/cmd/commands/generate"
 	_ "github.com/beego/bee/cmd/commands/hprose"
