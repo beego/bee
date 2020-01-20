a5c3a6e11a1cb81ce6c1f269d4bcb87e500bae2d
Alpha <AlphaWong@users.noreply.github.com>
2020-01-20 18:25:11 +0800

    Update new.go

--- cmd/commands/api/apiapp.go
+++ cmd/commands/api/apiapp.go
@@ -516,7 +516,7 @@ func init() {
 
 // TestGet is a sample to run an endpoint test
 func TestGet(t *testing.T) {
-	r, _ := http.NewRequest("GET", "/v1/object", nil)
+	r, _ := http.NewRequest(http.MethodGet, "/v1/object", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
 
