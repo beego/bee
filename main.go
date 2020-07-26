a68e8ae3e88c378430a1c2a35dd9a484bee9d4b0
Hanjiang Yu <delacroix.yu@gmail.com>
2020-07-26 10:41:52 +0800

    Parse AST even not in GOPATH

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func main() {
 
 			// Check if current directory is inside the GOPATH,
 			// if so parse the packages inside it.
-			if utils.IsInGOPATH(currentpath) && cmd.IfGenerateDocs(c.Name(), args) {
+			if cmd.IfGenerateDocs(c.Name(), args) {
 				swaggergen.ParsePackagesFromDir(currentpath)
 			}
 			os.Exit(c.Run(c, args))
