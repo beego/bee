99a25a316ab09a713c40b705796251aeb03d8c2d
RuanSong <ruansong200510@163.com>
2021-04-02 19:14:18 +0800

    support field comment if not defined description
     swaggergen support field comment if not defined description tag

--- generate/swaggergen/g_docs.go
+++ generate/swaggergen/g_docs.go
@@ -1187,6 +1187,8 @@ func parseStruct(imports []*ast.ImportSpec, st *ast.StructType, k string, m *swa
 					}
 					if desc := stag.Get("description"); desc != "" {
 						mp.Description = desc
+					} else {
+						mp.Description = strings.ReplaceAll(field.Comment.Text(), "\n", "")
 					}
 
 					if example := stag.Get("example"); example != "" && !isObject && !isSlice {
